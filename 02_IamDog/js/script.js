29b1e86cd671db8c544836249b4c0ea7422b9abf
webkida-ss <webkida.production@gmail.com>
2021-05-20 02:00:37 +0900

    IamDog基本作成 - HTML作成中

---
+++ 02_IamDog/js/script.js
@@ -0,0 +1,38 @@
+$(function () {
+
+	/*********************************************
+	 * スムーススクロール
+	 *********************************************/
+	$('a[href^="#"]').click(function () {
+		let header = $('.header').innerHeight();
+		let speed = 500;
+		let id = $(this).attr("href"); // 遷移先ID（href属性）
+		let target = $("#" == id ? "html" : id); // #のみだhtmlタグ（トップ）に戻る
+		let position = $(target).offset().top - header; // 固定ヘッダー分引く
+		// ヘッダーがfixedでない場合
+		if ($(".header").css("position") !== "fixed") {
+			position = $(target).offset().top;
+		}
+		if (position < 0) {
+			position = 0;
+		}
+		$("html, body").animate({
+				scrollTop: position
+			},
+			speed
+		);
+		return false;
+	});
+
+	/*********************************************
+	 * フローティングアイテム
+	 *********************************************/
+	$(window).on("scroll", function () {
+		if ($(this).scrollTop() > 200) {
+			$('.util__floating').addClass('is-show');
+		} else {
+			$('.util__floating').removeClass('is-show');
+		}
+	});
+
+})
